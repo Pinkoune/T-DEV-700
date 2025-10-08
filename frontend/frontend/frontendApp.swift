e9188d8769577bb8e8031adaa28d51a51876f720
Jérémy <76524822+Pinkoune@users.noreply.github.com>
2025-10-08 09:38:01 +0200

    [ADD] Dev branch

---
+++ frontend/frontend/frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  frontendApp.swift
+//  frontend
+//
+//  Created by Jérémy Barcelo on 08/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ frontend/frontendTests/frontendTests.swift
@@ -0,0 +1,17 @@
+//
+//  frontendTests.swift
+//  frontendTests
+//
+//  Created by Jérémy Barcelo on 08/10/2025.
+//
+
+import Testing
+@testable import frontend
+
+struct frontendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
