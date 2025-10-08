6fc8a888f4592ad23ea1cb3317a22c1c0c44a76d
David Vaucheret <davidvaucheret@MacBook-Pro-de-David.local>
2025-10-08 17:05:57 +0200

    Updated for dependencies

--- backend/Package.swift
+++ backend/Package.swift
@@ -7,14 +7,18 @@ let package = Package(
        .macOS(.v13)
     ],
     dependencies: [
-        // 💧 A server-side Swift web framework.
+        // A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.115.0"),
-        // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
+        // Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
-	// Leaf
-	.package(url: "https://github.com/vapor/leaf.git", from: "4.4.0"),
-	// VaporFirestore pour Firebase
-	.package(url: "https://github.com/PitchLabsAsh/vapor-firestore.git", from: "0.1.0"),
+        // Leaf
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.4.0"),
+        // Firebase SDK officiel
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.0.0"),
+        // dotenv package
+        .package(url: "https://github.com/thebarndog/swift-dotenv.git", from: "2.0.0"),
+        // JWT package
+        .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0"),
     ],
     targets: [
         .executableTarget(
@@ -23,8 +27,11 @@ let package = Package(
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
-		.product(name: "Leaf", package: "leaf"),
-		.product(name: "VaporFirestore", package: "vapor-firestore"),
+                .product(name: "Leaf", package: "leaf"),
+                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
+                .product(name: "SwiftDotenv", package: "swift-dotenv"),
+                .product(name: "JWT", package: "jwt"),
             ],
             swiftSettings: swiftSettings
         ),
