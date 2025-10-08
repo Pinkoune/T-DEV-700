be4c501dea216d0f7904add45023f218ab668ef6
David Vaucheret <davidvaucheret@MacBook-Pro-de-David.local>
2025-10-08 09:51:48 +0200

    Ajout dépendance Leaf et VaporFirestore

--- backend/Package.swift
+++ backend/Package.swift
@@ -11,6 +11,10 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.115.0"),
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+	// Leaf
+	.package(url: "https://github.com/vapor/leaf.git", from: "4.4.0"),
+	// VaporFirestore pour Firebase
+	.package(url: "https://github.com/PitchLabsAsh/vapor-firestore.git", from: "0.1.0"),
     ],
     targets: [
         .executableTarget(
@@ -19,6 +23,8 @@ let package = Package(
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
+		.product(name: "Leaf", package: "leaf"),
+		.product(name: "VaporFirestore", package: "vapor-firestore"),
             ],
             swiftSettings: swiftSettings
         ),
