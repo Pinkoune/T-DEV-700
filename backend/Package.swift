a53cc632855f05a2dd13a292af56212fd071bb5e
Davepich <david.vaucheret@epitech.eu>
2025-10-15 14:11:57 +0200

    resolving conflicts

--- backend/Package.swift
+++ backend/Package.swift
@@ -9,7 +9,19 @@ let package = Package(
     dependencies: [
         // A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.115.0"),
+<<<<<<< HEAD
         // JWT package for authentication
+=======
+        // Non-blocking, event-driven networking for Swift. Used for custom executors
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        // Leaf
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.4.0"),
+        // Firebase SDK officiel (commenté pour la démonstration)
+        // .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.0.0"),
+        // dotenv package
+        .package(url: "https://github.com/thebarndog/swift-dotenv.git", from: "2.0.0"),
+        // JWT package
+>>>>>>> 3cf58da (solution provisoire en rapport Firebase)
         .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0"),
         // Bcrypt package for secure password hashing
         .package(url: "https://github.com/vapor/bcrypt.git", from: "1.0.0"),
@@ -19,6 +31,14 @@ let package = Package(
             name: "backend",
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
+<<<<<<< HEAD
+=======
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+                .product(name: "Leaf", package: "leaf"),
+                // .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
+                .product(name: "SwiftDotenv", package: "swift-dotenv"),
+>>>>>>> 3cf58da (solution provisoire en rapport Firebase)
                 .product(name: "JWT", package: "jwt"),
                 .product(name: "BCrypt", package: "bcrypt"),
             ],
