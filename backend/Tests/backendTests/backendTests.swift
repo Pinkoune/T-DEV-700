9407a3ca45d301d61a7e619b88023bdad54b8d94
MathieuEx <mathieu.exposito@epitech.eu>
2025-10-16 16:18:34 +0200

    [FIX]I fix the test framework for the backend with the official framework for swift testing XCTVapor

--- backend/Tests/backendTests/backendTests.swift
+++ backend/Tests/backendTests/backendTests.swift
@@ -1,16 +1,16 @@
 @testable import backend
-import VaporTesting
-import Testing
+import XCTVapor
+import XCTest
 
-@Suite("App Tests")
-struct backendTests {
-    @Test("Test Hello World Route")
-    func helloWorld() async throws {
-        try await withApp(configure: configure) { app in
-            try await app.testing().test(.GET, "hello", afterResponse: { res async in
-                #expect(res.status == .ok)
-                #expect(res.body.string == "Hello, world!")
-            })
+final class AppTests: XCTestCase {
+    func testHealthCheck() async throws {
+        let app = try await Application.make(.testing)
+        defer { app.shutdown() }
+        
+        try await configure(app)
+        
+        try await app.test(.GET, "health") { res async in
+            XCTAssertEqual(res.status, .ok)
         }
     }
 }
