08b06b936a2d99e819bac883f4dcfe2d24fd1248
Timoty <timoty.viola@epitech.eu>
2025-12-15 15:00:09 +0100

    fix test file

--- backend/Tests/backendTests/backendTests.swift
+++ backend/Tests/backendTests/backendTests.swift
@@ -5,12 +5,18 @@ import XCTest
 final class AppTests: XCTestCase {
     func testHealthCheck() async throws {
         let app = try await Application.make(.testing)
-        defer { app.shutdown() }
         
-        try await configure(app)
-        
-        try await app.test(.GET, "health") { res async in
-            XCTAssertEqual(res.status, .ok)
+        do {
+            try await configure(app)
+            
+            try await app.test(.GET, "health") { res async in
+                XCTAssertEqual(res.status, .ok)
+            }
+        } catch {
+            try await app.asyncShutdown()
+            throw error
         }
+        
+        try await app.asyncShutdown()
     }
 }
